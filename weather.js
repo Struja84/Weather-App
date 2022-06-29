edbb0caa71f92d7fae2f70f1052b4871b6aa53c1
Struja84 <100865301+Struja84@users.noreply.github.com>
2022-06-29 23:59:41 +0200

    Update weather.js

--- weather.js
+++ weather.js
@@ -1,5 +1,5 @@
 const api1 = `73c2db8f4254bcb0c9d41d4e3d67a686`;                        // API  ENG // SRB  API
-const api2 = `aeeadc68f1596ffb15cc1e0ebd6c0520`;                        // API  ENG // SRB  API
+const api2 = `776537b1e7b4ac395c6d80afee24294e`;                        // API  ENG // SRB  API
 const wrapper = document.getElementById(`wrapper`);                    // WHOLE PAGE ENG //  SRB Celaa stranica hvatanje putem ID-ja
 
 const submit = document.getElementById(`submit`);                      // BUTTON ENG // SRB Dugme
@@ -38,32 +38,6 @@ window.onload = () => {
     // On load page focus on input
     document.getElementById(`search`).focus();
 }
-
-// function weatherCheck(data) {
-//     if(data.current.weather_descriptions[0] == 'Overcast') {
-//         wrapper.style.backgroundImage = `url(images/1.avif)`;
-//     }
-
-//     if(data.current.weather_descriptions[0] == 'Partly cloudy') {
-//         wrapper.style.backgroundImage = `url(images/Partly_cloud.jpg)`;
-//     }
-
-//     if(data.current.weather_descriptions[0] == 'Rain' || data.current.weather_descriptions[0] == 'Light rain shower' || data.current.weather_descriptions[0] == 'Light drizzle') {
-//         wrapper.style.backgroundImage = `url(images/2.jpg)`;
-//     }
-
-//     if(data.current.weather_descriptions[0] == 'Sunny' || data.current.weather_descriptions[0] == 'Clear') {
-//         wrapper.style.backgroundImage = `url(images/3.jpg)`;
-//     }
-
-//     if(data.current.weather_descriptions[0] == 'Light snow') {
-//         wrapper.style.backgroundImage = `url(images/4.jpg)`;
-//     }
-
-//     if(data.current.weather_descriptions[0] == 'Haze') {
-//         wrapper.style.backgroundImage = `url(images/Haze.jpg)`;
-//     }
-// };
 
 let favoriteCity = JSON.parse(localStorage.getItem('favoriteCity')) || [];
 
@@ -130,24 +104,38 @@ star.addEventListener(`click`, function(e) {
     wrongFormat.style.display = 'none';
     currentCity = document.getElementById(`search`).value;
 
-    if(favoriteCity.length == 0) {
-        saveFavorite();
-        updateFavoriteList();
-    }
-
-    favoriteCity.map((city) => {
-        if (favoriteCity.some(city => city.name === currentCity)) {
+    if(currentCity == '') {
+        wrongFormat.style.display = 'block';
+        wrongFormat.textContent = `Input is empty`;
+    } else {
+        if(!regExInput.test(currentCity)) {
+            wrongFormat.textContent = `Incorrect format search`;
             wrongFormat.style.display = 'block';
-            wrongFormat.textContent = `City is in favorites`;
-            // Trebalo bi da doda kada ima taj grad u favorites a da ga u elsu brise ali nesto ne radi
         } else {
-            saveFavorite();
-            updateFavoriteList();
             wrongFormat.style.display = 'none';
+            if(favoriteCity.length == 0) {
+                saveFavorite();
+                updateFavoriteList();
+            }
+        
+            if(favoriteCity.length == 5) {
+                wrongFormat.style.display = 'block';
+                wrongFormat.textContent = `Favorites full!`;
+            } else {
+                wrongFormat.style.display = 'none';
+                if (favoriteCity.some(city => city.name === currentCity)) {
+                    wrongFormat.style.display = 'block';
+                    wrongFormat.textContent = `City is in favorites`;
+                } else {
+                    saveFavorite();
+                    updateFavoriteList();
+                    wrongFormat.style.display = 'none';
+                }
+            }
         }
-    });
+    }
     document.getElementById(`search`).value = ``;
-    // document.getElementById(`search`).focus();
+    document.getElementById(`search`).focus();
 });
 
 // Submit vector for getting data after typing in input field
@@ -157,6 +145,8 @@ let regExInput = /^[A-Z][a-z]+( [a-zA-Z0-9_]+)*$/;
 submit.addEventListener(`click`, function(e) {
     currentCity = document.getElementById(`search`).value;
     e.preventDefault();
+    currentWeather.style.animation = `none`;
+    currentTime.style.animation = `none`;
     if(currentCity == '') {
         wrongFormat.style.display = 'block';
         wrongFormat.textContent = `Input is empty`;
@@ -170,6 +160,8 @@ submit.addEventListener(`click`, function(e) {
             requestCityData();
             requestCityDataApi2();
             document.getElementById(`search`).focus();
+            currentWeather.style.animation = `slideFromLeft 3s normal`;
+            currentTime.style.animation = `slideFromRight 3s normal`;
         }
     }
 });
@@ -287,7 +279,9 @@ let longitude = 0;
       
 
 geoLocation.addEventListener(`click`, function(e) {
-    e.preventDefault()
+    e.preventDefault();
+    currentWeather.style.animation = `none`;
+    currentTime.style.animation = `none`;
 
     // If we allowed browser to get our location call function success
 
@@ -328,6 +322,8 @@ geoLocation.addEventListener(`click`, function(e) {
             currentCity = `${rexText}, ${data.sys.country}`;
             requestCityData();
             wrongFormat.style.display = 'none';
+            currentWeather.style.animation = `slideFromLeft 3s normal`;
+            currentTime.style.animation = `slideFromRight 3s normal`;
         }).catch((err) => console.log(err));
     }
     document.getElementById(`search`).focus();
@@ -344,34 +340,44 @@ function requestCityDataApi2() {
 
 function checkWeatherDescDay() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${api1}`).then((response) => response.json()).then((data) => {
-        // console.log(data);
+        console.log(data);
         let weatherDesc = data.weather[0].main;
         if(weatherDesc == `Clear`) {
             wrapper.style.backgroundImage = `url(images/3.jpg)`;
             weatherIcon.src = `images/clear-day.svg`;
+            currentWeather.style.color = '';
+            currentTime.style.color ='';
         }
         if(weatherDesc == `Clouds`) {
             wrapper.style.backgroundImage = `url(images/Partly_cloud.jpg)`;
             weatherIcon.src = `images/partly-cloudy-day.svg`;
+            currentWeather.style.color = '';
+            currentTime.style.color ='';
         }
         if(weatherDesc == `Rain`) {
             wrapper.style.backgroundImage = `url(images/2.jpg)`;
             weatherIcon.src = `images/overcast-rain.svg`;
+            currentWeather.style.color = '';
+            currentTime.style.color ='';
         }
         if(weatherDesc == `Snow`) {
             wrapper.style.backgroundImage = `url(images/4.jpg)`;
             weatherIcon.src = `images/overcast-snow.svg`;
+            currentWeather.style.color = '';
+            currentTime.style.color ='';
         }
-        if(weatherDesc == `Dust`) {
+        if(weatherDesc == `Dust` || weatherDesc == 'Haze') {
             wrapper.style.backgroundImage = `url(images/Haze.jpg)`;
             weatherIcon.src = `images/overcast-haze.svg`;
+            currentWeather.style.color = '';
+            currentTime.style.color ='';
         }
     }).catch((err) => console.log(err));
 }
 
 function checkWeatherDescNight() {
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${api1}`).then((response) => response.json()).then((data) => {
-        // console.log(data);
+        console.log(data);
         let weatherDesc = data.weather[0].main;
         if(weatherDesc == `Clear`) {
             wrapper.style.backgroundImage = `url(images/clear_night.jpg)`;
@@ -397,11 +403,140 @@ function checkWeatherDescNight() {
             currentWeather.style.color = 'white';
             currentTime.style.color ='white';
         }
-        if(weatherDesc == `Dust`) {
+        if(weatherDesc == `Dust` || weatherDesc == 'Haze') {
             wrapper.style.backgroundImage = `url(images/Haze_night.jpg)`;
             weatherIcon.src = `images/dust.svg`;
             currentWeather.style.color = 'white';
             currentTime.style.color ='white';
         }
     }).catch((err) => console.log(err));
+}
+
+// Responsive JS
+
+const form = document.getElementsByTagName(`form`);
+const search = document.getElementById(`search`);
+const locationResponsive = document.getElementById(`location`);
+const searchCity = document.getElementById(`searchCity`);
+const displayWeather = document.getElementById(`displayWeather`);
+const divDetails = document.getElementsByClassName(`color`);
+const lastChild = document.getElementById(`lastChild`);
+const middleChild = document.getElementById(`middleChild`);
+
+const city = document.getElementsByClassName(`city`);
+// const cityRight = document.getElementsByClassName(`cityRight`);
+// console.log(city);
+
+// console.log(divDetails);
+
+window.addEventListener(`load`, responsive);
+window.addEventListener(`resize`, responsive);
+
+function responsive() {
+    const width = window.innerWidth;
+    console.log(width);
+    if(width < 1360) {
+        for (let i = 0; i < city.length; i++) {
+            city[i].style.display = `none`;  
+        }
+    } else {
+        for (let i = 0; i < city.length; i++) {
+            city[i].style.display = ``;  
+        }
+    }
+
+    if(width < 950) {
+        // Responsive input
+        form[0].style.width = `100%`;
+    } else {
+        // Responsive input
+        form[0].style.width = ``;
+    }
+
+    if(width < 665) {
+        for (let i = 0; i < city.length; i++) {
+            city[i].style.display = `inline`;
+        }
+        // Responsive DisplayWeather
+        min_max.style.fontSize = `1.5em`;
+        locationCity.style.fontSize = `2em`;
+        displayWeather.style.gridTemplateColumns = `1fr`;
+        displayWeather.style.height = `36vh`;
+        displayWeather.style.marginBottom = `2em`;
+
+        // Responsive input
+        form[0].style.display = `grid`;
+        form[0].style.gridTemplateColumns = `1fr 1fr 1fr`;
+
+        search.style.gridColumn = `span 4`;
+        search.style.width = `15em`;
+        search.style.margin = `0 auto`;
+
+        submit.style.display = `flex`;
+        submit.style.justifyContent = `flex-end`;
+
+        locationResponsive.style.display = `flex`;
+        locationResponsive.style.justifyContent = `flex-start`;
+
+        searchCity.style.height = `12vh`;
+
+        wrongFormat.style.width = `40vw`;
+        wrongFormat.style.padding = `0.4em`;
+        wrongFormat.style.top = `-2.1em`;
+
+        // Responsive Details
+        detailsOfCity.style.gridTemplateColumns = `1fr`;
+        detailsOfCity.style.height = `75vh`;
+        favs.style.padding = `1em`;
+        
+        for (let i = 0; i < divDetails.length; i++) {
+            divDetails[i].style.flexDirection = `row`;
+            divDetails[i].style.borderRight = `none`;
+            divDetails[i].style.borderBottom = `1px solid white`;
+            divDetails[i].style.alignItems = `center`;
+        }
+
+        middleChild.style.flexDirection = ``;
+        middleChild.style.padding = `0.5em`;
+        lastChild.style.borderBottom = `none`;
+        wrapper.style.height = `133vh`;
+
+    } else {
+        // Responsive DisplayWeather
+        min_max.style.fontSize = ``;
+        locationCity.style.fontSize = ``;
+        displayWeather.style.gridTemplateColumns = ``;
+        displayWeather.style.height = ``;
+        displayWeather.style.marginBottom = ``;
+
+        // Responsive input
+        form[0].style.display = ``;
+        form[0].style.gridTemplateColumns = ``;
+
+        search.style.gridColumn = ``;
+        search.style.width = ``;
+        search.style.margin = ``;
+
+        submit.style.display = ``;
+        submit.style.justifyContent = ``;
+
+        locationResponsive.style.display = ``;
+        locationResponsive.style.justifyContent = ``;
+
+        searchCity.style.height = ``;
+
+        // Responsive Details
+        detailsOfCity.style.gridTemplateColumns = ``;
+        detailsOfCity.style.height = ``;
+        favs.style.padding = ``;
+
+        for (let i = 0; i < divDetails.length; i++) {
+            divDetails[i].style.flexDirection = ``;
+            divDetails[i].style.borderRight = ``;
+            divDetails[i].style.borderBottom = ``;
+        }
+        middleChild.style.padding = ``;
+        lastChild.style.borderBottom = ``;
+        wrapper.style.height = ``;
+    }
 }
